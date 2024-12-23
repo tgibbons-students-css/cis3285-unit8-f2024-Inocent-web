339b82ac3cc5d6559c39706cd72630502ab640b5
Inocent Maturure <imaturure@css.edu>
2024-12-22 19:18:41 -0800

    Assignment 8

---
+++ TestUnit 8/Test1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using SingleResponsibilityPrinciple;
+using System.Data;
+using System.Reflection;
+
+namespace TestUnit_8
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private int CountDbRecords()
+        {
+            string azureConnectString = @"Server=tcp:cis3285-sql-server.database.windows.net,1433; Initial Catalog = Unit8_TradesDatabase; Persist Security Info=False; User ID=cis3285;Password=Saints4SQL; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 60;";
+            // Change the connection string used to match the one you want
+            using (var connection = new SqlConnection(azureConnectString))
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                string myScalarQuery = "SELECT COUNT(*) FROM trade";
+                SqlCommand myCommand = new SqlCommand(myScalarQuery, connection);
+                //myCommand.Connection.Open();
+                int count = (int)myCommand.ExecuteScalar();
+                connection.Close();
+                return count;
+            }
+        }
+
+        [TestMethod()]
+        public void TestNormalFile()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Unit8_SRP_F24Tests.goodtrades.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Act
+            int countBefore = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+            //Assert
+            int countAfter = CountDbRecords();
+            Assert.AreEqual(countBefore + 4, countAfter);
+        }
+        public void TestMethod1()
+        {
+        }
+    }
+}
